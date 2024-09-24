3c439d1956389877c21d35f95bb90a35d0f27aa5
Ritik220519 <ritik220519@gmail.com>
2024-09-24 14:07:38 +0530

    Create searchSugsestion

--- src/component/Head.js
+++ src/component/Head.js
@@ -1,17 +1,37 @@
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/redux/hamAppSlice";
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
-  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const toggleMenuHandler = ()=>{
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
     dispatch(toggleMenu());
-  }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSugsestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSugsestions = async () => {
+    console.log("API call -" + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    console.log(json[1])
+  };
   return (
     <div className="grid grid-flow-col p-1 m-2 shadow-md items-center">
       <div className="flex col-span-1 cursor-pointer">
         <img
-        onClick={()=>{toggleMenuHandler()}}
+          onClick={() => {
+            toggleMenuHandler();
+          }}
           className="h-14"
           src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-5.png"
           alt="hamburgger"
@@ -25,11 +45,15 @@ const Head = () => {
       </div>
       <div className="col-span-10 px-10">
         <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           className="w-1/2 border border-gray-500 bg-gray-50   p-2 rounded-l-full "
           type="text"
           placeholder="Search"
         />
-        <button className="border border-gray-500 bg-gray-200 px-4 py-2 rounded-r-full">🔍</button>
+        <button className="border border-gray-500 bg-gray-200 px-4 py-2 rounded-r-full">
+          🔍
+        </button>
       </div>
       <div className="col-span-1">
         <img
